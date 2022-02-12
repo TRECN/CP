14e99889e12914b0e0cf03415fb54f09f675b347
TRECN <rishabh1103@outlook.com>
2022-02-12 21:41:20 +0530

    commit

--- codes/shorteningCode.cpp
+++ codes/shorteningCode.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    l
+    //long long a = 123456789;
+    //long long b = 987654321;
+    //cout << a*b << "\n";
    
+    ll a = 123456789;
+    ll b = 987654321;
+    cout << a*b << "\n";
+    
 }
