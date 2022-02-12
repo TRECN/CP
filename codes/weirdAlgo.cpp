ce07339570e140086f806d29a616aec922acab9e
TRECN <rishabh1103@outlook.com>
2022-02-12 21:23:46 +0530

    commit

--- codes/weirdAlgo.cpp
+++ codes/weirdAlgo.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    while(n!=1){
+    while(true){
         cout<<n<<" ";
+        if(n==1)break;
         if(n%2==0)n/=2;
         else n=n*3+1;
     }
