dccce850fc9ee419c27a16d25489d6cffa4dabb1
TRECN <rishabh1103@outlook.com>
2022-11-05 01:52:59 +0530

    commit

---
+++ CodeForces/BitPP.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int n;
+    cin>>n;
+    int x=0;
+    while(n--){
+        string s;
+        cin>>s;
+        int a=s.size();
+        if(s[0]=='+'||s[a-1]=='+')
+            x++;
+        else    
+            x--;
+    }
+    cout<<x;
+    return 0;
+}
