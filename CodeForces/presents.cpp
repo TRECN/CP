6b5cc71e0d64ae5a9b35969d27c2368a4aa68c92
TRECN <rishabh1103@outlook.com>
2022-11-20 22:26:41 +0530

    commit

---
+++ CodeForces/presents.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int ans[n];
+    for(int i=0;i<n;i++){
+        ans[a[i]-1]=i+1;
+    }
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
