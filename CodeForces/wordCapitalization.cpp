0f5f3adbf0bf572bb33d306fd943ffceb0feb018
TRECN <rishabh1103@outlook.com>
2022-11-21 19:11:06 +0530

    commit

---
+++ CodeForces/wordCapitalization.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    string s;
+    cin>>s;
+    int n=s.size();
+    toupper(s[0]);
+    cout<<s;
+    return 0;
+}
